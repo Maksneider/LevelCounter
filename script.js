79a7e6a5ad27d4effe1834550a3457c14d0927ed
Maksymilian Wierzba <maks@MacBook-Air-Maksymilian.local>
2022-11-24 10:25:42 +0100

    before phone tests

---
+++ script.js
@@ -0,0 +1,34 @@
+var lvl=1;
+var lvlD=new Audio();
+lvlD.src="audio/lvlDown.mp3";
+var lvlU=new Audio();
+lvlU.src="audio/lvlUp.mp3";
+var rollSound= new Audio();
+rollSound.src="audio/roll.mp3";
+
+function increase() {
+  document.getElementById('level').innerHTML = ++lvl;
+  lvlU.play();
+  lvlU.currentTime=0;
+}
+
+function decrease() {
+  if(lvl>1){
+    document.getElementById('level').innerHTML = --lvl;
+    lvlD.play();
+    lvlD.currentTime=0;
+  }
+}
+
+function getRandomINT(min, max){
+  min= Math.ceil(min);
+  max= Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1) +min);
+}
+
+function roll(){
+  rollSound.play()
+  rollSound.currentTime=0;
+  let rolled = getRandomINT(1,6);
+  document.getElementById("rollNumb").innerHTML = rolled;
+}
